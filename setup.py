af288206f405cded63f944ba191be17d443b48c4
Rob Tucker <rtucker@mozilla.com>
2017-10-25 12:46:30 -0400

    Added requirements file

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(name='infoblox_axfr',
     author='Rob Tucker',
     author_email='rtucker@mozilla.com',
     packages=['infoblox_axfr'],
+    setup_requires = [
+        'dnspython==1.15'
+    ],
     entry_points={
         'console_scripts': [
             'infoblox_axfr=infoblox_axfr.main:main'
